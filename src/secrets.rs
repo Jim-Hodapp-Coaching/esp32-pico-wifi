5ec343e00da3523da6c3264e226befddf673b7ab
Jim Hodapp <james.hodapp@gmail.com>
2022-03-21 10:40:34 -0500

    Removes the Ambi server IP/port from main.rs and moves it into the configuration source file, secrets.rs

--- src/secrets.rs
+++ src/secrets.rs
@@ -5,3 +5,7 @@ use heapless::String;
 
 const SSID: &str = "SSID";
 const PASSPHRASE: &str = "PASSPHRASE";
+
+// Non-production Ambi server IP address/port pair for local network development
+const NON_PROD_AMBI_IP: [u8; 4] = [0, 0, 0, 0];
+const NON_PROD_AMBI_PORT: u16 = 4000;
